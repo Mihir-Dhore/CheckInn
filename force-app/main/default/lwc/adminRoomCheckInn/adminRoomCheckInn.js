b7bc190cdc020927ca8ffddbeaf2ac8e594e3a40
mukesh <sutharmukesh@gmail.com>
2024-03-08 15:02:55 +0530

    drag n drop on Admin Room Page

--- force-app/main/default/lwc/adminRoomCheckInn/adminRoomCheckInn.js
+++ force-app/main/default/lwc/adminRoomCheckInn/adminRoomCheckInn.js
@@ -4,8 +4,10 @@ import insertRoom from '@salesforce/apex/CheckInn.insertRoom';
 
 export default class AdminRoomCheckInn extends LightningElement {
     @track roomInfo;
-  
-    
+//   Drag and Drop -- START
+    @track dragStart;
+    @track ElementList = [];
+
     connectedCallback(){
         this.showRooms();
     }
@@ -21,11 +23,48 @@ export default class AdminRoomCheckInn extends LightningElement {
                 }
 
             });
+          // Initialize ElementList with roomInfo indices    
+            this.ElementList = Array.from(Array(this.roomInfo.length).keys());
+            console.log('this.ElementList ',JSON.stringify(this.ElementList))
         }).catch(error=>{
             console.log('error',error.message.body);
         })
     
     }
+
+    DragStart(event){
+        this.dragStart = event.target.title;
+        console.log('this.dragStart ',this.dragStart);
+    }
+    DragOver(event){
+        event.preventDefault();
+        return false;
+    }
+    Drop(event){
+        // event.preventDefault();
+        event.stopPropagation();
+        const dragValName = this.dragStart;
+        console.log('dragValName ',dragValName);
+
+        const dropValName = event.target.title;
+        console.log('dropValName ',dropValName);
+
+        if(dragValName===dropValName){
+            return false;
+        }
+
+        const index = dropValName;
+        console.log('index',index);
+        const currentIndex = parseInt(dragValName);
+        const newIndex = parseInt(dropValName);
+
+        console.log('currentIndex-',currentIndex , 'newIndex-',newIndex);
+        this.ElementList.splice(newIndex, 0, this.ElementList.splice(currentIndex, 1)[0]);
+
+        // Optionally, update the roomInfo order
+        this.roomInfo = this.ElementList.map(index => this.roomInfo[index]);
+    }
+//   Drag and Drop -- END
 
     //Insert Room
     @track showInsert = false;
